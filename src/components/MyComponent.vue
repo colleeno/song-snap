8d2414a6042da6fcf903accaac60ac1be0c38c45
colleeno <colleenaoreilly@gmail.com>
2024-01-30 17:05:40 -0500

    Adds axios with a test hit, adds env vars

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -1,3 +1,25 @@
 <template>
   <h1>hello</h1>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data () {
+    return {
+      clientId: import.meta.env.VITE_APP_SPOTIFY_API_ID,
+      clientSecret: import.meta.env.VITE_APP_SPOTIFY_CLIENT_SECRET
+    }
+  },
+  mounted () {
+    axios.get('https://jsonplaceholder.typicode.com/posts/1')
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+    });
+  },
+};
+</script>
